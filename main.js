903bc797520f62614b19c57cef8f1b3460ee0335
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-08-28 16:14:03 -0500

    feat: :sparkles: Portafolio

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("owo");
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.profline', { delay: 300 });
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 1000 });
